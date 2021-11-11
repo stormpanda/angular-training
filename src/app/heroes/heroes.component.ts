deecb74572d16ab620773da9eebb1996c1e94115
stormpanda <maikthomalla@gmail.com>
2021-11-11 10:16:39 +0100

    make heroes from list selectable and editable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,19 +5,18 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
-  constructor() { }
+  selectedHero?: Hero;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
