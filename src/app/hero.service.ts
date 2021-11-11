a918275de9f5085a0e7e3ff6fff6936861c899f0
stormpanda <maikthomalla@gmail.com>
2021-11-11 11:39:07 +0100

    use observable for list of heroes in hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,8 @@ import { Hero } from './hero';
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
