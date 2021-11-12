d19fc2b221922feb3a8e249284973b62742b0434
stormpanda <maikthomalla@gmail.com>
2021-11-12 15:26:13 +0100

    use behavior subject and observable for message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
-  messages: string[] = [];
+  private messagesSubject = new BehaviorSubject<string[]>([]);
+  messages$ = this.messagesSubject.asObservable();
 
   add(message: string) {
-    this.messages.push(message);
+    this.messagesSubject.next([...this.messagesSubject.getValue(), message]);
   }
 
   clear() {
-    this.messages = [];
+    this.messagesSubject.next([]);
   }
 }
